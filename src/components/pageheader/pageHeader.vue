ffdd94fac9c9998f18137131d9974b063170dbed
Wenxuan Liu <553858245@qq.com>
2022-06-13 12:51:37 +0800

    Initial commit

---
+++ src/components/pageheader/pageHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <div style="display: flex;font-size: 20px;font-weight: 500;cursor:pointer;">
+    <div style="display: flex;" @click="goback()">
+      <div><i class="el-icon-back"></i></div>
+      <div style="margin: 0 20px">返回</div>
+    </div>
+    <div>{{ name }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "pageHeader",
+  props: ['name'],
+  methods: {
+    goback() {
+      this.$emit('goBack')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/common/home.vue
@@ -0,0 +1,224 @@
+<template>
+  <div class="mod-demo-echarts">
+    <el-alert
+      title="提示："
+      type="warning"
+      :closable="false">
+      <div slot-scope="description">
+        <p class="el-alert__description">1. 此Demo只提供ECharts官方使用文档，入门部署和体验功能。具体使用请参考：http://echarts.baidu.com/index.html</p>
+      </div>
+    </el-alert>
+
+    <el-row :gutter="20">
+      <el-col :span="12">
+        <el-card>
+          <div id="J_chartPieBox" class="chart-box"></div>
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card>
+          <div id="J_chartScatterBox" class="chart-box"></div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import echarts from 'echarts'
+export default {
+  data () {
+    return {
+      chartPie: null,
+      chartPie1:null,
+
+    }
+  },
+  mounted () {
+    this.initChartPie()
+    this.initChartPie1()
+  },
+  activated () {
+    // 由于给echart添加了resize事件, 在组件激活时需要重新resize绘画一次, 否则出现空白bug
+    if (this.chartPie) {
+      this.chartPie.resize()
+    }
+    if (this.chartScatter) {
+      this.chartScatter.resize()
+    }
+  },
+  methods: {
+    // 饼状图
+    initChartPie () {
+      var option = {
+        backgroundColor: '#2c343c',
+        title: {
+          text: 'Customized Pie',
+          left: 'center',
+          top: 20,
+          textStyle: {
+            color: '#ccc'
+          }
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        visualMap: {
+          show: false,
+          min: 80,
+          max: 600,
+          inRange: {
+            colorLightness: [0, 1]
+          }
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '50%'],
+            data: [
+              { value: 335, name: '直接访问' },
+              { value: 310, name: '邮件营销' },
+              { value: 274, name: '联盟广告' },
+              { value: 235, name: '视频广告' },
+              { value: 400, name: '搜索引擎' }
+            ].sort(function (a, b) { return a.value - b.value }),
+            roseType: 'radius',
+            label: {
+              normal: {
+                textStyle: {
+                  color: 'rgba(255, 255, 255, 0.3)'
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                lineStyle: {
+                  color: 'rgba(255, 255, 255, 0.3)'
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20
+              }
+            },
+            itemStyle: {
+              normal: {
+                color: '#c23531',
+                shadowBlur: 200,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            },
+            animationType: 'scale',
+            animationEasing: 'elasticOut',
+            animationDelay: function (idx) {
+              return Math.random() * 200
+            }
+          }
+        ]
+      }
+      this.chartPie = echarts.init(document.getElementById('J_chartPieBox'))
+      this.chartPie.setOption(option)
+      window.addEventListener('resize', () => {
+        this.chartPie.resize()
+      })
+    },
+    // 散点图
+    initChartPie1 () {
+      var option = {
+        backgroundColor: '#2c343c',
+        title: {
+          text: 'Customized Pie',
+          left: 'center',
+          top: 20,
+          textStyle: {
+            color: '#ccc'
+          }
+        },
+        tooltip: {
+          trigger: 'item',
+          formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        visualMap: {
+          show: false,
+          min: 80,
+          max: 600,
+          inRange: {
+            colorLightness: [0, 1]
+          }
+        },
+        series: [
+          {
+            name: '访问来源',
+            type: 'pie',
+            radius: '55%',
+            center: ['50%', '50%'],
+            data: [
+              { value: 335, name: '直接访问' },
+              { value: 310, name: '邮件营销' },
+              { value: 274, name: '联盟广告' },
+              { value: 235, name: '视频广告' },
+              { value: 400, name: '搜索引擎' }
+            ].sort(function (a, b) { return a.value - b.value }),
+            roseType: 'radius',
+            label: {
+              normal: {
+                textStyle: {
+                  color: 'rgba(255, 255, 255, 0.3)'
+                }
+              }
+            },
+            labelLine: {
+              normal: {
+                lineStyle: {
+                  color: 'rgba(255, 255, 255, 0.3)'
+                },
+                smooth: 0.2,
+                length: 10,
+                length2: 20
+              }
+            },
+            itemStyle: {
+              normal: {
+                color: '#c23531',
+                shadowBlur: 200,
+                shadowColor: 'rgba(0, 0, 0, 0.5)'
+              }
+            },
+            animationType: 'scale',
+            animationEasing: 'elasticOut',
+            animationDelay: function (idx) {
+              return Math.random() * 200
+            }
+          }
+        ]
+      }
+      this.chartPie1 = echarts.init(document.getElementById('J_chartScatterBox'))
+      this.chartPie1.setOption(option)
+      window.addEventListener('resize', () => {
+        this.chartPie1.resize()
+      })
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+.mod-demo-echarts {
+  > .el-alert {
+    margin-bottom: 10px;
+  }
+  > .el-row {
+    margin-top: -10px;
+    margin-bottom: -10px;
+    .el-col {
+      padding-top: 10px;
+      padding-bottom: 10px;
+    }
+  }
+  .chart-box {
+    min-height: 400px;
+  }
+}
+</style>
